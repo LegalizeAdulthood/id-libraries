298725276625881c493e4a3529988401dd287529
Richard <legalize@xmission.com>
2025-12-22 10:40:18 -0700

    Update validate-all.bat to validate IFS and L-system files

--- scripts/validate-all.bat
+++ scripts/validate-all.bat
@@ -2,3 +2,5 @@
 setlocal
 for %%f in (*.frm) do @validate-frm.py -q %%f
 for %%f in (*.par) do @validate-par.py -q %%f
+for %%f in (*.ifs) do @validate-ifs.py -q %%f
+for %%f in (*.l) do @validate-lsys.py -q %%f
